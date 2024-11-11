a855703c753b3d31f1a20b512a81667c1af1b486
Lucky Nkolonzi <35879963+nkolonzilucky@users.noreply.github.com>
2024-11-11 22:07:41 +0000

    added tailwind extensions

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,11 +9,47 @@ export default {
   ],
   theme: {
     extend: {
+      screens: {
+        xs: "475px"
+      },
       colors: {
+        primary: {
+          "100": "#FFE8F0",
+          DEFAULT: "#EE2B69",
+        },
+        secondary: "#FBE843",
+        black: {
+          "100": "#333333",
+          "200": "#141413",
+          "300": "#7D8087",
+          DEFAULT: "#000000"
+        },
+        white: {
+          "100": "#F7F7F7",
+          DEFAULT: "#FFFFFF"
+        },
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      fontFamily: {
+        "work-sans": ["var(--font-work-sans)"],
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)"
+      },
+      boxShadow: {
+        100: "2px 2px 0px rgb(0, 0, 0)",
+        200: "2px 2px 0px rgb(0, 0, 0)",
+        300: "2px 2px 0px rgb(238, 43, 105)",
+      }
     },
   },
-  plugins: [require('tailwindcss-motion'), require('daisyui')],
+  plugins: [
+    require('tailwindcss-motion'), 
+    require('tailwindcss-animate'), 
+    require('daisyui'),
+    require("@tailwindcss/typography")
+  ],
 } satisfies Config;
