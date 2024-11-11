a5cb33bb4c975939508f41de254ae792e17b9865
Lucky Nkolonzi <35879963+nkolonzilucky@users.noreply.github.com>
2024-11-11 04:27:54 +0000

    Started auth setup

---
+++ app/(root)/page.tsx
@@ -0,0 +1,10 @@
+import { HomeIcon } from '@heroicons/react/24/outline'
+
+export default function Home() {
+  return (
+    <div className='flex items-center space-x-1'>
+     <HomeIcon className='size-5' /> 
+    <h1 className="text-2xl">Home</h1>
+    </div>
+  );
+}
--- app/page.tsx
+++
@@ -1,7 +0,0 @@
-import Image from "next/image";
-
-export default function Home() {
-  return (
-    <h1 className="text-4xl">Hello World <span className="hover:motion-preset-bounce hover:motion-duration-2000 hover:cursor-pointer active:motion-preset-fade">👋🏿</span></h1>
-  );
-}
