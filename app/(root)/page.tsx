5126135f2071e44b507cdb3902ece6478f7eba32
Lucky Nkolonzi <35879963+nkolonzilucky@users.noreply.github.com>
2024-11-12 10:22:15 +0000

    extended a tailwind utilities with a custom heading class for reusability

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,11 @@
 import { HomeIcon } from '@heroicons/react/24/outline'
 
+
 export default function Home() {
   return (
-    <div className='flex items-center space-x-1'>
-     <HomeIcon className='size-5' /> 
-    <h1 className="text-2xl">Home</h1>
+    <div className='flex items-center justify-center space-x-0'>
+     <HomeIcon className='size-9' /> 
+    <h1 className="heading">Home</h1>
     </div>
   );
 }
