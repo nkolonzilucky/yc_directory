b19ef05e5a4ecccb8ea03231371430c00789c068
Lucky Nkolonzi <35879963+nkolonzilucky@users.noreply.github.com>
2024-11-18 09:22:06 +0000

    Added a header

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,9 +3,13 @@ import { HomeIcon } from '@heroicons/react/24/outline'
 
 export default function Home() {
   return (
-    <div className='flex items-center justify-center space-x-0'>
-     <HomeIcon className='size-9' /> 
-    <h1 className="heading">Home</h1>
-    </div>
+    <>
+    <section className="pink_container">
+      <h1 className="heading">
+        Pitch Your Startup <hr />
+        Connect with Entrepreneurs
+      </h1>
+    </section>
+    </>
   );
 }
