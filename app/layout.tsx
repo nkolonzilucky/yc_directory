cec82b2229b9a548ba206a195e3029f079fccac2
Lucky Nkolonzi <35879963+nkolonzilucky@users.noreply.github.com>
2024-11-20 09:38:20 +0000

    comment

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import 'easymde/dist/easymde.min.css';
+import 'easymde/dist/easymde.min.css'; // relates to sanity
 
 const workSans = localFont({
   src: [
