d21585ac70f5e5b1bdf5ac677d9f4594edd3a792
Lucky Nkolonzi <35879963+nkolonzilucky@users.noreply.github.com>
2024-11-21 23:03:39 +0000

    added details of the details page

--- app/(root)/startup/[id]/page.tsx
+++ app/(root)/startup/[id]/page.tsx
@@ -1,7 +1,12 @@
 import { client } from '@/sanity/lib/client';
 import { STARTUP_BY_ID_QUERY } from '@/sanity/lib/queries';
+import Image from 'next/image';
+import Link from 'next/link';
 import { notFound } from 'next/navigation';
 import React from 'react'
+import markdownit from 'markdown-it'
+
+const md = markdownit();
 
 export const experimental_ppr = true;
 
@@ -10,6 +15,8 @@ const page = async ({ params }: { params: Promise<{ id: string }>}) => {
     const post = await client.fetch(STARTUP_BY_ID_QUERY, { id });
 
     if (!post) return notFound();
+
+    const parsedContent = md.render(post?.pitch || '');
   return (
     <>
     <section className='pink_container !min-h-[230px]'>
@@ -24,11 +31,35 @@ const page = async ({ params }: { params: Promise<{ id: string }>}) => {
       />
       <div className="space-y-5 mt-10 max-w-4xl mx-auto">
         <div className="flex-between gap-5">
-          
+          <Link href={`/user/${post.author?._id}`} className='flex gap-2 items-center mb-3'>
+            <Image 
+              src={post.author?.image}
+              alt='avatar'
+              width={64}
+              height={64}
+              className='rounded-full drop-shadow-lg'
+            />
+            <div>
+              <p className="text-20-medium">{post.author?.name}</p>
+              <p className="text-20-medium !text-black-100">
+                @{post.author?.username}
+              </p>
+            </div>
+          </Link>
+          <p className='category-tag'>{post.category}</p>
         </div>
-
+        <h3 className='text-30-bold'>
+          Pitch Details
+        </h3>
+        {parsedContent ? (
+          <article className='prose max-w-4xl font-work-sans break-all'
+            dangerouslySetInnerHTML={{ __html: parsedContent }} />
+        ) : (<p className='no-result'>No details provided</p>)}
       </div>
+      <hr className='divider' />
+      {/* TOD: EDITOR SELECTED STARTUPS */}
     </section>
+    
     </>
   )
 }
